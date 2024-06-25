9e958e4b4fe2ee7400a5025a1ae5297ef5734d55
Vivek <vivekpradhan736@gmail.com>
2024-06-25 19:00:48 +0530

    new commit

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -37,7 +37,7 @@ import ChatLoading from "./ChatLoading";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal.js";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chatapp-backend-or0g.onrender.com"; // -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain, fetchProps }) => {
